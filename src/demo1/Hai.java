fb78e4349e9875cff235bee657364f6e599d3f08
Administrator <Administrator@SKY-20190102LCR>
2019-09-04 11:11:25 +0800

    asd

--- src/demo1/Hai.java
+++ src/demo1/Hai.java
@@ -1,11 +1,11 @@
 package demo1;
 
-import java.util.concurrent.SynchronousQueue;
 
 public class Hai {
 	public static void main(String[] args) {
 		System.out.println("hai");
 		System.out.println("haiaia");
-                System.out.println("ss");
+        System.out.println("ss");
+        
 	}
 }
